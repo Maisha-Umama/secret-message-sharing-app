983e797feb504a442bdddd6c5e864588efcba7a4
Maisha Umama <umamamaisha@gmail.com>
2020-09-09 22:46:01 +0600

    finshed

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+const { hash }= window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message){
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+}
+
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();//to stop the default browser behaviour which is to submit the form
- 
+    
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
   
-    document.querySelector('#link-input').value = encrypted;
+    const LinkInput = document.querySelector('#link-input');
+    LinkInput.value =`${window.location}#${ encrypted}`;
+    LinkInput.select();
 });
